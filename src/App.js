e895fa9ca721926d25ff5b18b947182ddc03698e
williamsg12 <williamsg1@outlook.com>
2022-02-28 20:15:43 -0500

    added stuff

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App">  
+      <div>
+        <h1>Welcome</h1>
+      </div>
+
+      <div className='logo'>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
     </div>
   );
 }
